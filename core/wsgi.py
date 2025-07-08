926d2afbc22ff66a367fdc4b638a6b010b2d460d
slotys <marcoaranasloty@gmail.com>
2025-07-08 09:25:34 +0800

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -14,3 +14,10 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
+import os, sys
+print("DJANGO_SETTINGS_MODULE:", os.getenv("DJANGO_SETTINGS_MODULE"))
+print("PYTHONPATH:", "\n".join(sys.path))
+print("Existe core/__init__.py?", os.path.exists(os.path.join(sys.path[0], "core", "__init__.py")))
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
